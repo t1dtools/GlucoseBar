c2af3129b18d5e0e5e14bff326b96eb47aa28c78
Andreas Stokholm <andreas@stokholm.me>
2024-08-19 13:10:47 +0200

    Simpler code for drop color choice

--- GlucoseBar/GlucoseBarApp.swift
+++ GlucoseBar/GlucoseBarApp.swift
@@ -120,6 +120,10 @@ struct GlucoseBarApp: App {
         }
 
         let configuration = NSImage.SymbolConfiguration(pointSize: 16, weight: .light)
+        if applyColor {
+            configuration.applying(.init(paletteColors: [color]))
+        }
+
         let image = NSImage(systemSymbolName: symbol, accessibilityDescription: accessibilityDescription)
         let titleImage = image?.withSymbolConfiguration(configuration)!
 
