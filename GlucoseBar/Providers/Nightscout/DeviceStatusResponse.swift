6903a52c64d2731085469056bdfd7aa61ec42aad
tdrkDev <teledurak@gmail.com>
2025-10-10 14:51:05 +0700

    providers: Add AAPS/OpenAPS support

--- GlucoseBar/Providers/Nightscout/DeviceStatusResponse.swift
+++ GlucoseBar/Providers/Nightscout/DeviceStatusResponse.swift
@@ -17,8 +17,9 @@ struct DeviceStatusResponse: Codable {
 struct DeviceStatusResult: Codable {
 //    let pump: Pump
 //    let uploader: Uploader
+    let app: String?
     let device: String
-    let openaps: Openaps
+    let openaps: Openaps?
 //    let createdAt: String
 //    let utcOffset: Int
 //    let identifier: String
@@ -26,7 +27,7 @@ struct DeviceStatusResult: Codable {
 
     enum CodingKeys: String, CodingKey {
 //        case pump, uploader, device, openaps
-        case device, openaps
+        case app,device, openaps
 //        case createdAt = "created_at"
 //        case utcOffset, identifier, srvModified, srvCreated
     }
@@ -34,43 +35,59 @@ struct DeviceStatusResult: Codable {
 
 // MARK: - Openaps
 struct Openaps: Codable {
-    let iob: Iob
-    let version: String
-    let suggested, enacted: Ted
+    let iob: Iob?
+    //let version: String
+    let suggested: Ted?
+    let enacted: Ted?
 }
 
 // MARK: - Ted
 struct Ted: Codable {
-    let insulinForManualBolus: Double
-    let received: Bool
+    let insulinForManualBolus: Double?
+    let received: Bool?
     let sensitivityRatio: Double
     let insulinReq: Double
-    let minDelta, reservoir, tdd: Double
+    let minDelta: Double?
+    let reservoir: Double?
+    let tdd: Double?
     let reason: String
-    let manualBolusErrorString, currentTarget, duration: Double
-    let expectedDelta: Double
+    let manualBolusErrorString, currentTarget: Double?
+    let duration: Double?
+    let expectedDelta: Double?
     let timestamp: String
     let bg: Double
-    let id: String
-    let eventualBG, threshold: Double
+    let id: String?
+    let eventualBG: Double
+    let threshold: Double?
     let cob: Double?
     let iob: Double?
     let rate: Double
-    let deliverAt, temp: String
+    let deliverAt: String
+    let temp: String?
     let predBGs: PredBGs
-    let isf, cr: Double
+    let isf: Double
+    let cr: Double?
 
     enum CodingKeys: String, CodingKey {
         case insulinForManualBolus
         case iob = "IOB"
-        case received, sensitivityRatio, insulinReq, minDelta, reservoir
+        case received, sensitivityRatio, insulinReq
+        case minDelta
+        case reservoir
         case tdd = "TDD"
-        case reason, manualBolusErrorString
+        case reason
+        case manualBolusErrorString
         case currentTarget = "current_target"
-        case duration, expectedDelta, timestamp, bg, id, eventualBG
+        case duration
+        case expectedDelta
+        case timestamp, bg
+        case id
+        case eventualBG
         case cob = "COB"
-        case threshold, rate, deliverAt, temp, predBGs
-        case isf = "ISF"
+        case threshold, rate, deliverAt
+        case temp
+        case predBGs
+        case isf = "variable_sens"
         case cr = "CR"
     }
 }
@@ -90,13 +107,15 @@ struct PredBGs: Codable {
 // MARK: - Iob
 struct Iob: Codable {
     let basaliob: Double
-    let lastTemp: LastTemp
-    let netbasalinsulin, iob: Double
+    let lastTemp: LastTemp?
+    let netbasalinsulin: Double?
+    let iob: Double
     let time: String
-    let lastBolusTime: Int
-    let bolusiob: Double
-    let iobWithZeroTemp: IobWithZeroTemp
-    let activity, bolusinsulin: Double
+    let lastBolusTime: Int?
+    let bolusiob: Double?
+    let iobWithZeroTemp: IobWithZeroTemp?
+    let activity: Double
+    let bolusinsulin: Double?
 }
 
 // MARK: - IobWithZeroTemp
@@ -131,20 +150,20 @@ struct Pump: Codable {
 
 // MARK: - Battery
 struct Battery: Codable {
-    let display: Bool
-    let percent: Int
-    let string: String
+    let display: Bool?
+    let percent: Int?
+    let string: String?
 }
 
 // MARK: - Status
 struct Status: Codable {
-    let bolusing: Bool
+    let bolusing: Bool?
     let timestamp, status: String
-    let suspended: Bool
+    let suspended: Bool?
 }
 
 // MARK: - Uploader
 struct Uploader: Codable {
-    let battery: Int
-    let isCharging: Bool
+    let battery: Int?
+    let isCharging: Bool?
 }
--- GlucoseBar/Providers/Nightscout/GlucoseSource.swift
+++ GlucoseBar/Providers/Nightscout/GlucoseSource.swift
@@ -10,6 +10,8 @@ import Foundation
 public enum GlucoseSourceDevice: String, CaseIterable, Identifiable, Sendable {
     case null
     case trio
+    case openaps
+    case aaps
     public var id: String { self.rawValue }
     public var presentable: String {
         switch self {
@@ -17,7 +19,26 @@ public enum GlucoseSourceDevice: String, CaseIterable, Identifiable, Sendable {
             return "No extras"
         case .trio:
             return "Trio"
+        case .openaps:
+            return "OpenAPS"
+        case .aaps:
+            return "AAPS"
         }
+    }
+    
+    static func fromDS(status: DeviceStatusResult) -> GlucoseSourceDevice {
+        if status.device == "Trio" {
+            return .trio
+        } else if status.device.starts(with: "openaps://") {
+            if (status.app == "AAPS") {
+                return .aaps
+            } else {
+                return .openaps
+            }
+        } else {
+            return .null
+        }
+        
     }
 }
 
@@ -65,45 +86,37 @@ class GlucoseSource: Nightscout, @unchecked Sendable {
             if res!.statusCode != 200 {
                 return .null
             }
-
+            
             do {
                 let result = try JSONDecoder().decode(DeviceStatusResponse.self, from: data)
-
-                if result.result.first!.device == "Trio" {
-                    let enacted = result.result.first!.openaps.enacted
+    
+                let firstResult = result.result.first
+                if firstResult == nil {
+                    self.logger.error("No result found in DeviceStatus")
+                    return .null
+                }
+                
+                let device = GlucoseSourceDevice.fromDS(status: firstResult!)
+                if device != GlucoseSourceDevice.null {
+                    let enacted = firstResult!.openaps?.enacted ?? firstResult!.openaps?.suggested
+                    if enacted == nil {
+                        self.logger.error("No enacted/suggested found in DeviceStatus")
+                        return .null
+                    }
 
                     var gsep = GlucoseSourceExtraProperties()
-                    gsep.cob = enacted.cob ?? 0
-                    gsep.iob = enacted.iob ?? 0
-                    gsep.eventualGlucose = enacted.eventualBG
-                    gsep.reason = enacted.reason
-                    
-                    var oapsf = OpenAPSForecasts()
-                    if enacted.predBGs.iob != nil {
-                        oapsf.iob = enacted.predBGs.iob
-                    }
-                    
-                    if enacted.predBGs.cob != nil {
-                        oapsf.cob = enacted.predBGs.cob
-                    }
-                    
-                    if enacted.predBGs.uam != nil {
-                        oapsf.uam = enacted.predBGs.uam
-                    }
-                    
-                    if enacted.predBGs.zt != nil {
-                        oapsf.zt = enacted.predBGs.zt
-                    }
-                    gsep.forecasts = oapsf
-                    gsep.glucoseTarget = enacted.currentTarget
+                    gsep.cob = enacted!.cob ?? 0
+                    gsep.iob = enacted!.iob ?? 0
+                    gsep.eventualGlucose = enacted!.eventualBG
+                    gsep.reason = enacted!.reason
+                    gsep.forecasts = OpenAPSForecasts.fromPredBGs(predBGs: enacted!.predBGs)
+                    gsep.glucoseTarget = enacted!.currentTarget
                     
                     DispatchQueue.global().sync {
                         self.GlucoseSourceExtras = gsep
                     }
-
-                    return .trio
                 }
-                return .null
+                return device
             } catch {
                 self.logger.error("Unable to decode NS response when checking for GSE: \(String(describing: error))")
                 return .null
@@ -148,34 +161,35 @@ class GlucoseSource: Nightscout, @unchecked Sendable {
 
             do {
                 let result = try JSONDecoder().decode(DeviceStatusResponse.self, from: data)
-
-                if result.result.first!.device == "Trio" {
-                    let enacted = result.result.first!.openaps.enacted
-                    var forecasts = OpenAPSForecasts()
-                    if enacted.predBGs.iob != nil {
-                        forecasts.iob = enacted.predBGs.iob
-                    }
-
-                    if enacted.predBGs.cob != nil {
-                        forecasts.cob = enacted.predBGs.cob
-                    }
-
-                    if enacted.predBGs.zt != nil {
-                        forecasts.zt = enacted.predBGs.zt
-                    }
-
-                    if enacted.predBGs.uam != nil {
-                        forecasts.uam = enacted.predBGs.uam
+                
+                let firstResult = result.result.first
+                if firstResult == nil {
+                    self.logger.error("No result found in DeviceStatus")
+                    return empty
+                }
+                
+                let device = GlucoseSourceDevice.fromDS(status: firstResult!)
+                if device != GlucoseSourceDevice.null {
+                    let enacted = firstResult!.openaps?.enacted ?? firstResult!.openaps?.suggested
+                    if enacted == nil {
+                        self.logger.error("No enacted/suggested found in DeviceStatus")
+                        return empty
                     }
 
                     let dateFormatter = DateFormatter()
                     dateFormatter.locale = Locale(identifier: "en_US_POSIX")
                     dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
                     dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 0)
-                    
-                    let ts = dateFormatter.date(from: enacted.deliverAt)
 
-                    return GlucoseSourceExtraProperties(iob: enacted.iob, cob: enacted.cob, eventualGlucose: enacted.eventualBG, reason: enacted.reason, enactedAt: ts, forecasts: forecasts, glucoseTarget: enacted.currentTarget)
+                    return GlucoseSourceExtraProperties(
+                        iob: enacted!.iob,
+                        cob: enacted!.cob,
+                        eventualGlucose: enacted!.eventualBG,
+                        reason: enacted!.reason,
+                        enactedAt: dateFormatter.date(from: enacted!.deliverAt),
+                        forecasts: OpenAPSForecasts.fromPredBGs(predBGs: enacted!.predBGs),
+                        glucoseTarget: enacted!.currentTarget
+                    )
                 }
             } catch {
                 self.logger.error("Unable to decode NS response when checking for GSE: \(String(describing: error))")
